4f83289700b266157a0feff2208b53a75696a9f1
Joyce Zhou <9123137+cephcyn@users.noreply.github.com>
2020-12-09 00:54:41 -0800

    add new bookmark data to make scraping more streamlined

--- scraper.py
+++ scraper.py
@@ -27,18 +27,23 @@ def filter_posts(post_df):
 # Collect approximately `min_posts` number of posts (or the total # of posts in subreddit, whichever is smaller)
 # Collect posts from `subreddit`
 # Output the data file to `output_filename`
-def scrape_subreddit(min_posts, subreddit):
+def scrape_subreddit(min_posts, subreddit, start_utc=None):
     # get the sub creation date
     with urllib.request.urlopen(f"https://api.pushshift.io/reddit/search/submission/?subreddit={subreddit}&size=1&sort=asc&before=30d") as url:
         data = json.loads(url.read().decode())
     df_sub = pd.DataFrame.from_dict(pd.json_normalize(data['data']), orient='columns')
     created_utc_first_sub = df_sub.tail(1)['created_utc']
+    # set a default value for created_utc_now_sub
+    created_utc_now_sub = created_utc_first_sub
 
     # get the most recent N posts
     with urllib.request.urlopen(f"https://api.pushshift.io/reddit/search/submission/?subreddit={subreddit}&size=1&sort=desc&before=30d") as url:
         data = json.loads(url.read().decode())
     df_sub = pd.DataFrame.from_dict(pd.json_normalize(data['data']), orient='columns')
-    created_utc_now_sub = df_sub.tail(1)['created_utc']
+    if start_utc is None:
+        created_utc_now_sub = df_sub.tail(1)['created_utc']
+    else:
+        created_utc_now_sub = start_utc
     
     # make sure we get at least args.post_count number of posts scraped
     # but also don't go past the beginning of the subreddit
@@ -58,22 +63,35 @@ def scrape_subreddit(min_posts, subreddit):
         df_sub = df_sub[~df_sub.index.duplicated(keep='first')]
         df_sub = df_sub[['author', 'author_flair_text', 'created_utc', 'permalink', 'retrieved_on', 
                          'score', 'subreddit', 'title', 'upvote_ratio', 'url']]
-        return df_sub
+        return df_sub, created_utc_now_sub
 
 try:
     if os.path.isfile('data/dataset.h5'):
-        print(f'loading from file!')
+        print(f'loading post data from file!')
         dataset_df = pd.read_hdf('data/dataset.h5', 'df')
     else:
-        print(f'no file to load from!')
+        print(f'no post data file to load from!')
         dataset_df = pd.DataFrame()
+        
+    if os.path.isfile('data/utcbookmarks.pickle'):
+        print(f'loading utc bookmarks from file!')
+        with open('data/utcbookmarks.pickle', 'rb') as f:
+            utc_now_dict = pickle.load(f)
+    else:
+        print(f'no bookmark file to load from!')
+        utc_now_dict = {}
     
     for sub_name in sub_list:
         num_collected = len(dataset_df[dataset_df.subreddit.str.lower() == sub_name.lower()])
         if num_collected < num_posts_each_sub:
             num_to_collect = num_posts_each_sub - num_collected
             print(f'currently scraping sub r/{sub_name}: need {num_to_collect}')
-            scraped_df = scrape_subreddit(num_to_collect, sub_name)
+            if sub_name in utc_now_dict:
+                # if we have a backup, save that
+                scraped_df, created_utc_now = scrape_subreddit(num_to_collect, sub_name, start_utc=utc_now_dict[sub_name])
+            else:
+                scraped_df, created_utc_now = scrape_subreddit(num_to_collect, sub_name)
+            utc_now_dict[sub_name] = created_utc_now
             dataset_df = pd.concat([dataset_df, scraped_df])
             print(f'     done scraping sub r/{sub_name}')
 finally:
@@ -81,3 +99,5 @@ finally:
     if os.path.isfile('data/dataset.h5'):
         os.remove('data/dataset.h5')
     dataset_df.to_hdf('data/dataset.h5', key='df')
+    with open('data/utcbookmarks.pickle', 'wb') as f:
+        pickle.dump(utc_now_dict, f)
