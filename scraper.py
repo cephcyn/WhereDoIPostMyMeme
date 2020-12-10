be6ffe0b6f5ad8e713d2c2996fa1f1f07a86dd89
Joyce Zhou <9123137+cephcyn@users.noreply.github.com>
2020-12-09 18:31:13 -0800

    update scraper to be more parallelizable-ish-maybe

--- scraper.py
+++ scraper.py
@@ -7,13 +7,34 @@ import pickle
 pickle.HIGHEST_PROTOCOL = 4
 
 # list of subreddits to collect posts from
-sub_list = ['greentext', 'DeepFriedMemes', 'me_irl', '2meirl4meirl', 'dankmemes', 
-            'adviceanimals', 'dogelore', 'wholesomememes', 'CatsPlayingDnd', 'glitch_art', 
-            'awwnime', 'EarthPorn', 'tumblr', 'NapkinMemes', 'chickenswearingpants', 
-            'BreadStapledToTrees', 'terriblefacebookmemes', 'Animemes', 'dataisbeautiful']
+sub_list = [
+    'greentext',             # DONE
+    'DeepFriedMemes',        # DONE
+    'me_irl',                # file0
+    '2meirl4meirl',          # file0
+    'dankmemes',             # file0
+    'adviceanimals',         # file0
+    'dogelore',              # file0
+    'wholesomememes',        # file0
+    'CatsPlayingDnd',        # file1
+    'glitch_art',            # file1
+    'awwnime',               # file1
+    'EarthPorn',             # file1
+    'tumblr',                # file1
+    'NapkinMemes',           # file2
+    'chickenswearingpants',  # file2
+    'BreadStapledToTrees',   # file2
+    'terriblefacebookmemes', # file2
+    'Animemes',              # file2
+    'dataisbeautiful',       # file2
+]
 
 # number of posts to collect from each sub
 num_posts_each_sub = 5000
+
+fnum = ''
+output_filename_df = f'data/{fnum}dataset.h5'
+output_filename_bookmark = f'data/{fnum}utcbookmarks.pickle'
 
 def filter_posts(post_df):
     output_df = post_df
@@ -29,7 +50,8 @@ def filter_posts(post_df):
 # Output the data file to `output_filename`
 def scrape_subreddit(min_posts, subreddit, start_utc=None):
     # get the sub creation date
-    with urllib.request.urlopen(f"https://api.pushshift.io/reddit/search/submission/?subreddit={subreddit}&size=1&sort=asc&before=30d") as url:
+    url = f"https://api.pushshift.io/reddit/search/submission/?subreddit={subreddit}&size=1&sort=asc&before=30d"
+    with urllib.request.urlopen(urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})) as url:
         data = json.loads(url.read().decode())
     df_sub = pd.DataFrame.from_dict(pd.json_normalize(data['data']), orient='columns')
     created_utc_first_sub = df_sub.tail(1)['created_utc']
@@ -37,7 +59,8 @@ def scrape_subreddit(min_posts, subreddit, start_utc=None):
     created_utc_now_sub = created_utc_first_sub
 
     # get the most recent N posts
-    with urllib.request.urlopen(f"https://api.pushshift.io/reddit/search/submission/?subreddit={subreddit}&size=1&sort=desc&before=30d") as url:
+    url = f"https://api.pushshift.io/reddit/search/submission/?subreddit={subreddit}&size=1&sort=desc&before=30d"
+    with urllib.request.urlopen(urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})) as url:
         data = json.loads(url.read().decode())
     df_sub = pd.DataFrame.from_dict(pd.json_normalize(data['data']), orient='columns')
     if start_utc is None:
@@ -49,7 +72,8 @@ def scrape_subreddit(min_posts, subreddit, start_utc=None):
     # but also don't go past the beginning of the subreddit
     try:
         while (len(df_sub.index) < min_posts) and (int(created_utc_now_sub) > int(created_utc_first_sub)): 
-            with urllib.request.urlopen(f"https://api.pushshift.io/reddit/search/submission/?subreddit={subreddit}&size=1000&sort=desc&before=%d"%created_utc_now_sub) as url:
+            url = f"https://api.pushshift.io/reddit/search/submission/?subreddit={subreddit}&size=1000&sort=desc&before=%d"%created_utc_now_sub
+            with urllib.request.urlopen(urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})) as url:
                 data = json.loads(url.read().decode())
             df_new_sub = pd.DataFrame.from_dict(pd.json_normalize(data['data']), orient='columns')
             df_sub = df_sub.append(df_new_sub)
@@ -66,16 +90,16 @@ def scrape_subreddit(min_posts, subreddit, start_utc=None):
         return df_sub, created_utc_now_sub
 
 try:
-    if os.path.isfile('data/dataset.h5'):
+    if os.path.isfile(output_filename_df):
         print(f'loading post data from file!')
-        dataset_df = pd.read_hdf('data/dataset.h5', 'df')
+        dataset_df = pd.read_hdf(output_filename_df, 'df')
     else:
         print(f'no post data file to load from!')
         dataset_df = pd.DataFrame()
         
-    if os.path.isfile('data/utcbookmarks.pickle'):
+    if os.path.isfile(output_filename_bookmark):
         print(f'loading utc bookmarks from file!')
-        with open('data/utcbookmarks.pickle', 'rb') as f:
+        with open(output_filename_bookmark, 'rb') as f:
             utc_now_dict = pickle.load(f)
     else:
         print(f'no bookmark file to load from!')
@@ -96,8 +120,10 @@ try:
             print(f'     done scraping sub r/{sub_name}')
 finally:
     print(f'rewriting/dumping output to file!')
-    if os.path.isfile('data/dataset.h5'):
-        os.remove('data/dataset.h5')
-    dataset_df.to_hdf('data/dataset.h5', key='df')
-    with open('data/utcbookmarks.pickle', 'wb') as f:
+    if os.path.isfile(output_filename_df):
+        os.remove(output_filename_df)
+    dataset_df.to_hdf(output_filename_df, key='df')
+    if os.path.isfile(output_filename_bookmark):
+        os.remove(output_filename_bookmark)
+    with open(output_filename_bookmark, 'wb') as f:
         pickle.dump(utc_now_dict, f)
