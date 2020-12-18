7ed1e12fb89181450c7e25500df82cac802f424f
Joyce Zhou <9123137+cephcyn@users.noreply.github.com>
2020-12-18 03:01:53 -0800

    updated memenet website to be on react, contain all components of previous html site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,243 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 
-function App() {
+import 'react-aspect-ratio/aspect-ratio.css'
+import AspectRatio from 'react-aspect-ratio';
+import Carousel from 'react-material-ui-carousel'
+
+import "fontsource-roboto"
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    align: 'center',
+  },
+  panel: {
+    padding: '15px 30px',
+    marginTop: '20px',
+    marginBottom: '20px',
+  },
+  examplecard: {
+    padding: '10px 30px',
+    position: 'relative',
+    left: '50%',
+    transform: 'translate(-50%, 0)',
+    maxWidth: '80%',
+  },
+  detailtext: {
+    padding: '10px 30px',
+    marginTop: '15px',
+    marginBottom: '15px',
+  },
+  shiny: {
+    // I wonder if I can randomize the color lmao
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+  },
+}));
+
+export default function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className={classes.root}>
+      <CssBaseline />
+      <Paper className={classes.panel} style={{ textAlign:'center' }}>
+        <Typography variant="h1">
+          MemeNet
+        </Typography>
+        <Typography variant="h4">
+          Multimodal Models Make Meme Market Manageable
+        </Typography>
+      </Paper>
+      <Paper className={classes.panel}>
+        <Typography>
+          Artificial Intelligence (A.I.) has been applied in areas such as
+          economics and algorithmic trading to great effect. In recent decades,
+          the rise of viral Internet culture has led to the development of a new
+          global economy: the online "meme economy". Drawing from scarce resources
+          (such as creativity, humor, and time), individual producers (meme
+          makers) offer their goods (memes in the form of multimodal ideas) over a
+          centralized marketplace (Internet forums such as subreddits on Reddit)
+          in exchange for currency (Internet points such as Upvotes or Likes).
+          Oftentimes, knowing <em>where</em> to post a meme can greatly affect how
+          well it is received by the Internet community. Posting in a highly apt
+          channel can lead to instant Internet fame, while posting in a suboptimal
+          channel can lead to one's creative work failing to gain attention, or
+          worse, being stolen and reposted by meme thieves. Additionally, posting
+          the same content in several different channels can be considered
+          &quot;spamming&quot; and is negatively regarded. To make this decision easier for
+          the millions of meme creators on the Internet, <strong>we developed a
+          multimodal neural network to predict the single best subreddit that a
+          given meme should be posted to for maximum profit</strong>.
+        </Typography>
+      </Paper>
+      <Paper className={classes.panel} style={{ textAlign:'center' }}>
+        <AspectRatio ratio="16 / 9" style={{ maxWidth: '60%', left: '50%', transform: 'translate(-50%, 0)' }}>
+          <iframe
+            src="https://www.youtube-nocookie.com/embed/bZe5J8SVCYQ"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen>
+          </iframe>
+        </AspectRatio>
+      </Paper>
+      <Paper className={classes.panel}>
+        <Typography variant="h4" style={{ textAlign:'center' }} gutterBottom>
+          Examples
+        </Typography>
+        <div style={{ maxWidth: '80%', position: 'relative', left: '50%', transform: 'translate(-50%, 0)' }}>
+          <Carousel
+            autoPlay={false}
+            animation={"slide"}
+            indicators={true}
+            timeout={500}
+            navButtonsAlwaysVisible={true}
+            navButtonsAlwaysInvisible={false}
+          >
+            {
+              [
+                <Paper className={classes.examplecard}>
+                  <Typography>
+                    PAGE 1:
+                  </Typography>
+                  <img
+                    style={{ width:'256px', 'height':'256px' }}
+                    src="https://i.pinimg.com/474x/7c/4d/15/7c4d1533480bb4c5911d95699fef5186.jpg"
+                  />
+                </Paper>,
+                <Paper className={classes.examplecard}>
+                  <Typography>
+                    PAGE 2:
+                  </Typography>
+                  <img
+                    style={{ width:'256px', 'height':'256px' }}
+                    src="https://i.pinimg.com/474x/1b/e4/94/1be494c3c065c9c97da3231f7303ee85.jpg"
+                  />
+                </Paper>,
+                <Paper className={classes.examplecard}>
+                  <Typography>
+                    PAGE 3:
+                  </Typography>
+                  <img
+                    style={{ width:'256px', 'height':'256px' }}
+                    src="https://i.pinimg.com/564x/32/58/ba/3258bafbefbc616c5736347001d9bb15.jpg"
+                  />
+                </Paper>
+              ]
+            }
+          </Carousel>
+        </div>
+      </Paper>
+      <Paper className={classes.panel}>
+        <Typography variant="h4" style={{ textAlign:'center' }} gutterBottom>
+          Try It Yourself!
+        </Typography>
+        <Button className={classes.shiny}>
+          Hello World
+        </Button>
+      </Paper>
+      <Paper className={classes.panel}>
+        <Typography variant="h4" style={{ textAlign:'center' }} gutterBottom>
+          Behind The Scenes
+        </Typography>
+        <Paper className={classes.detailtext}>
+          <Typography variant="h5" style={{ textAlign:'center' }} gutterBottom>
+            Abstract
+          </Typography>
+          <Typography>
+            Deep neural networks are excellent at learning from data that consists
+            of single modalities. For example, convolutional neural networks are
+            highly performant on image classification, and sequence models are the
+            state-of-the-art for text generation. However, media such as Internet
+            memes often consist of multiple modalities. A meme may have an image
+            component and a text component, each of which contribute information
+            about what the meme is trying to convey. To extract features from
+            multimodal data, we leverage multimodal deep learning, in which we use
+            multiple feature extractor networks to learn the separate modes
+            individually, and an aggregator network to combine the features to
+            produce the final output classification. We scrape Reddit meme
+            subreddits for post data, including: subreddit name, upvote/downvote
+            count, images, meme text via OCR (or human OCR), and post titles. We
+            construct a train and test set and evaluate results using a
+            precision/accuracy measure for subreddit name predictions. To optimize
+            our model, we use FAIR’s open source multimodal library, Pythia/MMF
+            (<a href="https://mmf.sh/" rel="nofollow">https://mmf.sh/</a>), and try
+            a variety of model architectures and hyperparameters. Finally, we include
+            our best model for demonstration purposes.
+          </Typography>
+        </Paper>
+        <Paper className={classes.detailtext}>
+          <Typography variant="h5" style={{ textAlign:'center' }} gutterBottom>
+            Methodology
+          </Typography>
+          <Typography>
+            Bla bla bla Blaaaaa bla bla blah blaah blah Blah blaaah Blaaah Bla bla Blah
+            blah Blah blaaah Blaaah Bla bla Blahbla bla blah blaah blah blaaah Blaaah
+            Bla bla bla Blaaaaa bla bla blah blaah blah Blah blaaah Blaaah Bla bla Blah
+            blah Blah blaaah Blaaah Bla bla Blahbla bla blah blaah blah blaaah Blaaah
+            Bla bla bla Blaaaaa bla bla blah blaah blah Blah blaaah Blaaah Bla bla Blah
+            blah Blah blaaah Blaaah Bla bla Blahbla bla blah blaah blah blaaah Blaaah
+            Bla bla bla Blaaaaa bla bla blah blaah blah Blah blaaah Blaaah Bla bla Blah
+            blah Blah blaaah Blaaah Bla bla Blahbla bla blah blaah blah blaaah Blaaah
+          </Typography>
+        </Paper>
+        <Paper className={classes.detailtext}>
+          <Typography variant="h5" style={{ textAlign:'center' }} gutterBottom>
+            Experiments
+          </Typography>
+          <Typography>
+            Bla bla bla Blaaaaa bla bla blah blaah blah Blah blaaah Blaaah Bla bla Blah
+            blah Blah blaaah Blaaah Bla bla Blahbla bla blah blaah blah blaaah Blaaah
+            Bla bla bla Blaaaaa bla bla blah blaah blah Blah blaaah Blaaah Bla bla Blah
+            blah Blah blaaah Blaaah Bla bla Blahbla bla blah blaah blah blaaah Blaaah
+            Bla bla bla Blaaaaa bla bla blah blaah blah Blah blaaah Blaaah Bla bla Blah
+            blah Blah blaaah Blaaah Bla bla Blahbla bla blah blaah blah blaaah Blaaah
+            Bla bla bla Blaaaaa bla bla blah blaah blah Blah blaaah Blaaah Bla bla Blah
+            blah Blah blaaah Blaaah Bla bla Blahbla bla blah blaah blah blaaah Blaaah
+          </Typography>
+        </Paper>
+        <Paper className={classes.detailtext}>
+          <Typography variant="h5" style={{ textAlign:'center' }} gutterBottom>
+            Results
+          </Typography>
+          <Typography>
+            Bla bla bla Blaaaaa bla bla blah blaah blah Blah blaaah Blaaah Bla bla Blah
+            blah Blah blaaah Blaaah Bla bla Blahbla bla blah blaah blah blaaah Blaaah
+            Bla bla bla Blaaaaa bla bla blah blaah blah Blah blaaah Blaaah Bla bla Blah
+            blah Blah blaaah Blaaah Bla bla Blahbla bla blah blaah blah blaaah Blaaah
+            Bla bla bla Blaaaaa bla bla blah blaah blah Blah blaaah Blaaah Bla bla Blah
+            blah Blah blaaah Blaaah Bla bla Blahbla bla blah blaah blah blaaah Blaaah
+            Bla bla bla Blaaaaa bla bla blah blaah blah Blah blaaah Blaaah Bla bla Blah
+            blah Blah blaaah Blaaah Bla bla Blahbla bla blah blaah blah blaaah Blaaah
+          </Typography>
+        </Paper>
+      </Paper>
+      <Paper className={classes.panel}>
+        <Typography variant="h4" style={{ textAlign:'center' }} gutterBottom>
+          Related Work
+        </Typography>
+        <Typography>
+          Bla bla bla Blaaaaa bla bla blah blaah blah Blah blaaah Blaaah Bla bla Blah
+          blah Blah blaaah Blaaah Bla bla Blahbla bla blah blaah blah blaaah Blaaah
+          Bla bla bla Blaaaaa bla bla blah blaah blah Blah blaaah Blaaah Bla bla Blah
+          blah Blah blaaah Blaaah Bla bla Blahbla bla blah blaah blah blaaah Blaaah
+          Bla bla bla Blaaaaa bla bla blah blaah blah Blah blaaah Blaaah Bla bla Blah
+          blah Blah blaaah Blaaah Bla bla Blahbla bla blah blaah blah blaaah Blaaah
+          Bla bla bla Blaaaaa bla bla blah blaah blah Blah blaaah Blaaah Bla bla Blah
+          blah Blah blaaah Blaaah Bla bla Blahbla bla blah blaah blah blaaah Blaaah
+        </Typography>
+      </Paper>
+    </Container>
   );
 }
-
-export default App;
